e9136485ee6ac71c267ce216c1cceb66dc196109
John Kelly <kellyjra@gmail.com>
2022-01-06 16:04:13 +0000

    adding a simple Student Service that returns a mocked up student object

---
+++ src/main/kotlin/jk/codespace/restapi/service/StudentService.kt
@@ -0,0 +1,18 @@
+package jk.codespace.restapi.service
+
+import jk.codespace.restapi.dto.Student
+import mu.KotlinLogging
+import org.springframework.stereotype.Service
+
+@Service
+class StudentService {
+    private val log = KotlinLogging.logger {}
+
+    fun getStudent(studentId: String) : Student {
+        log.info {"Attempting to get student :  $studentId" }
+        return Student(
+            studentId = studentId,
+        firstName = "Joe",
+        lastName = "Bloggs")
+    }
+}
