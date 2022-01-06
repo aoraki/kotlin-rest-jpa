c82951a028cc4743fc6f442dda89cfd9fada45dd
John Kelly <kellyjra@gmail.com>
2022-01-06 16:01:12 +0000

    adding kotlin logging dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,16 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	// Required for Kotlin dev
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	// Logging
+	implementation("io.github.microutils:kotlin-logging:2.1.21")
+
+	// Testing dependencies
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
