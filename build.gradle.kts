ab1f98549bff5ac9611f95bfe726dc42255747eb
John Kelly <kellyjra@gmail.com>
2022-01-19 17:11:31 +0000

    adding dependencies to support DB persistence

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("plugin.jpa") version "1.4.32"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 }
@@ -22,6 +23,13 @@ dependencies {
 	// Required for Kotlin dev
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	// DB Related dependencies
+	runtimeOnly("com.h2database:h2")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	// Mapping DTOs to JPA Entities
+	implementation("org.modelmapper:modelmapper:2.4.5")
 
 	// Logging
 	implementation("io.github.microutils:kotlin-logging:2.1.21")
